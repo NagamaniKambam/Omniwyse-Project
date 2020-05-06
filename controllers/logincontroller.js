2d3f937c0bc2ab83c9c4f85ba768fd97a2dbd011
Kalyan <kalyankumarreddy098@gmail.com>
2020-05-06 11:19:44 +0530

    updated

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -1,16 +1,8 @@
 
 var Users = require('../models/users');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 exports.login = function(req,res){
     var uname = req.body.username;
     var pass = req.body.password;
-
-  //  bcrypt.genSalt(saltRounds, (err, salt) => {
-  //      bcrypt.hash(yourPassword, salt, (err, hash) => {
-            // Now we can store the password hash in db.
-  //      });
-  //  });
 
     Users.findOne({email_id : uname,password : pass},function(err,user){
         if(err){
